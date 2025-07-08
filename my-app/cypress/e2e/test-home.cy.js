0fd65206371137e2553936c0376d3b6b89a703b4
alucrece <vanelle-lucrece.aliebong-zeukeng@epitech.eu>
2025-07-08 10:42:51 +0200

    fixer le test cypress

--- my-app/cypress/e2e/test-home.cy.js
+++ my-app/cypress/e2e/test-home.cy.js
@@ -1,6 +1,6 @@
 describe("Homepage", () => {
   it("loads the home page", () => {
     cy.visit("http://localhost:3000");
-    cy.contains("Welcome").should("exist");
+    cy.contains("Formulaire d'inscription").should("exist");
   });
 });
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,8 @@ import React from "react";
 import RegistrationForm from "./components/registrationForm";
 import axios from "axios";
 import { useState, useEffect } from "react";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 function App() {
   const port = process.env.REACT_APP_SERVER_PORT;
@@ -14,17 +16,29 @@ function App() {
           baseURL: `http://localhost:${port}`
         });
         const response = await api.get(`/users`);
-        setUsersCount (response.data.utilisateurs.length)
+        setUsersCount(response.data.utilisateurs.length);
       } catch (error) {
         console.error(error);
       }
     }
-    countUsers()
-  }, [port])
+    countUsers();
+  }, [port]);
+
   return (
     <div>
       <h1>Formulaire d'inscription</h1>
       <RegistrationForm />
+      
+      {/* Ceci est obligatoire pour voir les toasts */}
+      <ToastContainer
+        position="top-right"
+        autoClose={3000}
+        hideProgressBar={false}
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+        draggable
+      />
     </div>
   );
 }
